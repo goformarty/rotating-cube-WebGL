950367d9525a7a202869abcc133b232d5ceed6e6
Martyna.Stepien <martyna.stepien@ldntranml003722.local>
2017-10-16 14:51:56 +0100

    vertex and fragment shader initialised, buffer initalised

--- main.js
+++ main.js
@@ -12,4 +12,93 @@ function main() {
 
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	// vertex shader
+	const vsSource = `
+		attribute vec4 aVertexPosition;
+
+		uniform mat4 uModelViewMatrix;
+		uniform mat4 uProjectionMatrix;
+
+		void main() {
+			gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+		}
+	`;
+
+	// fragment shader 
+	const fsSource = `
+		void main() {
+			gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+		}
+	`;
+
+	function initShaderProgram(gl, vsSource, fsSource) {
+		const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+		const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+		const shaderProgram = gl.createProgram();
+		gl.attachShader(shaderProgram, vertexShader);
+		gl.attachShader(shaderProgram, fragmentShader);
+		gl.linkProgram(shaderProgram);
+	}
+
+	// if creating shader program failed, alert
+	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+		alert('Unable to initialise shader programe: ' + gl.getProgramInfoLog(shaderProgram));
+		return null;
+	}
+
+	return shaderProgram;
+}
+
+function loadShader(gl, type, source) {
+	const shader = gl.createShader(type);
+
+	gl.shaderSource(shader, source);
+
+	gl.compileShader(shader);
+
+	// check if compiled successfully 
+	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		alert('An error occured compiling the shaders: ' + gl.getShaderInfoLog(shader));
+		gl.deleteShader(shader);
+		return null;
+	}
+
+	return shader;
+}
+
+const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+
+const programInfo = {
+	program: shaderProgram,
+	attribLocations: {
+		vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+	},
+	uniformLocations: {
+		projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+		modelViewMatrix: gl.getUniforamLocation(shaderProgram, 'uModelViewMatrix'),
+	},
+};
+
+function initBuffers(gl) {
+	const positionBuffer = gl.createBuffer();
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+	// an array of positions for the square
+	const positions = [
+		1.0, 1.0, -1.0, 1.0,
+		1.0, -1.0, -1.0, -1.0,
+	];
+
+	// pass the list of position into webGL by creating a Float32Array from the JavaScript array
+	// then use it to fill the current buffer
+
+	gl.bufferData(gl.ARRAY_BUFFER,
+		new Float32Array(positions),
+		gl.STATIC_DRAW);
+		return {
+			position: positionBuffer,
+		};
 }
